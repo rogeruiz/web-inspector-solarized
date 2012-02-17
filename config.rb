4a612f080a5cebe8e16f64a3e18387ded51db62c
Roger Steve Ruiz <contact@rogeruiz.com>
2012-02-17 01:47:14 -0500

    updated the config

--- config.rb
+++ config.rb
@@ -1,15 +1,8 @@
-# Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = ""
-
-# You can select your preferred output style here (can be overridden via the command line):
-output_style = :compact
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
+output_style = :compact #:nested :expanded :compressed
 relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
